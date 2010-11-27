612915914a88d0e0ff7a515fbd74110d0d39ad16
Glenn Willen <gwillen@nerdnet.org>
2010-11-27 14:41:37 -0500

    world, level loader

---
+++ World.hx
@@ -0,0 +1,19 @@
+enum TileType {
+  nothing;
+  floor;
+  movingfloor;
+  floorstop;  // Displays and acts like nothing, but stops moving floors
+  butan;  // Pressed or unpressed
+  man;
+  turtal;
+}
+
+class MovingFloor {
+  var id:int ;  // id of linked butan
+}
+
+class Tile {
+  static var size = 20; // width and height
+  var type:TileType;
+}
+
