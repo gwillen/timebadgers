495f5277a0675f6e68c1fefe28c8b4300413f9ee
David Renshaw <dwrenshaw@gmail.com>
2010-11-27 13:00:55 -0500

    stringofs

--- haxetest/Jump.hx
+++ haxetest/Jump.hx
@@ -28,25 +28,43 @@ overthree = jmp (3,0) [(1,0), (2,0)]
 
 */
 
+  static function readjmp(a : Array<Coor>) : Jmp {
+    if(a.length < 1 )
+      { // bad input
+        return {dest : {x:0,y:0}, path : []};
+      }
+    var d: Coor = a[0];
+    return {dest : d, path : a.slice(1)};
+  }
+
   static var upone : Jmp = { dest : {x:0, y:-1}, path : []  } ;
   static var uptwo : Jmp = { dest : {x:0, y:-2}, path : [ {x:0,y:-1}]  } ;
   static var upthree : Jmp = { dest : {x:0, y:-3}, path : [{x:0,y:-1},{x:0,y:-2}] } ;
 
   static var rightupone : Jmp = { dest: {x:1,y:-1}, path : [{x:0,y:-1}]};
-  static var rightuptwo  : Jmp = {dest : {x:1,y:-2}, path : [{x:0,y:-1}, {x:0,y:-2}]};
-  
+  static var rightuptwo  : Jmp = { dest : {x:1,y:-2}, path : [{x:0,y:-1}, {x:0,y:-2}]};
+//  static var rightupthree : Jmp = readjmp();
 
   static var arr: Array<Int>  = [1,2,3,4,5,6];
 
-  static function printjmp(j : Jmp) {
-    trace ("dest = " + j.dest.x + ", " + j.dest.y);
+  static function stringofcoor(c : Coor){
+    return  ("(" + c.x + "," + c.y  + ")");
+  } 
+
+  static function stringofjmp(j : Jmp) {
+    var d =  ( "dest= " + stringofcoor(j.dest) + " path = ");
+    for( i in 0...j.path.length) {
+         d = d + (stringofcoor(j.path[i])  );
+    }
+    return d;
   } 
 
 
-  static function main() {
-    printjmp(upthree);
 
-    trace ("Hello world!" + arr + "   " + upone);
+  static function main() {
+    trace ( stringofjmp(upthree ));
+
+    trace ("Hello world!" + arr[0] + "   " + "length=" + arr.length );
     var mc:flash.display.MovieClip = flash.Lib.current;
     // type annotation not necessary
     mc.graphics.beginFill(0xFF0000);
