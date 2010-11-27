31323d7fe83c6ad3ad427e2923348a5859a32428
Glenn Willen <gwillen@nerdnet.org>
2010-11-27 15:42:23 -0500

    add more tile functionality, and some utilities

--- World.hx
+++ World.hx
@@ -1,4 +1,19 @@
-class World {}
+import Utils.Option;
+import Utils;
+import flash.net.URLRequest;
+import flash.display.Loader;
+
+class World {
+  public static var tile1;
+  public static var tile2;
+  public static function loadStuff() {
+    tile1 = new Loader();
+    tile1.load(new URLRequest("assets/tile_dummy1.jpg"));
+    tile2 = new Loader();
+    tile2.load(new URLRequest("assets/tile_dummy2.jpg"));
+  }
+  var worldState: Array<Array<Tile>>;
+}
 
 enum TileType {
   nothing;
@@ -11,11 +26,18 @@ enum TileType {
 }
 
 class MovingFloor {
-  //var id:int;  // id of linked butan
+  var id:Option<Int>;  // id of linked butan
 }
 
 class Tile {
   static var size = 20; // width and height
   var type:TileType;
+  function getImage() {
+    if (Math.random() > 0.5) {
+      return World.tile1.content;
+    } else {
+      return World.tile2.content;
+    }
+  }
 }
 
