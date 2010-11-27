a99b1f378966a3b737bed301a5647f274e9b0d4e
Chris Martens <chrisamaphone@gmail.com>
2010-11-27 12:42:51 -0500

    drew some stuff

---
+++ Game.hx
@@ -0,0 +1,52 @@
+import flash.display.MovieClip;
+import flash.display.Graphics;
+
+enum TileType {
+  nothing;
+  floor;
+  man;
+  turtal;
+}
+
+class Tile {
+  static var size = 20; // width and height
+  var type:TileType;
+}
+
+class Game {
+  static var mc : MovieClip;
+  // game parameters
+  static var screenw = 600;
+  static var screenh = 600;
+
+  static var tiles : Array<Tile>;
+
+  static function drawTiles() {
+
+  }
+
+  static function main() {
+    mc = flash.Lib.current;
+
+    // 1. Read in the level file
+    // 2. Parse it into arrays of tiles
+    tiles = new Array<Tile>();
+
+    // 3. Draw the tiles
+    for ( i in 0...30 ) {
+      for (j in 0...30 ) {
+        var tileno = 30*i + j;
+        var color = 0xFF0000;
+        switch(tileno % 3) {
+          case 1 : color =  0xFFFFFF;
+          case 2 : color =  0x000000;
+        }
+        mc.graphics.beginFill(color);
+        mc.graphics.moveTo(i*20,j*20);
+        mc.graphics.lineTo(i*20,j*20+20);
+        mc.graphics.lineTo(i*20+20,j*20);
+        mc.graphics.endFill();
+      }
+    }
+  }
+}
