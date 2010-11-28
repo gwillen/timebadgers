f4c6fde7fb19ae64eb6c662cd5788bd541510912
David Renshaw <dwrenshaw@gmail.com>
2010-11-28 08:48:59 -0500

    oops youll need this file

---
+++ DebugTextField.hx
@@ -0,0 +1,27 @@
+import flash.display.MovieClip;
+import flash.display.Sprite;
+import flash.text.TextField;
+import flash.display.Graphics;
+import flash.Lib;
+import flash.display.Loader;
+import flash.net.URLRequest;
+import flash.display.Sprite;
+import flash.display.Bitmap;
+
+class DebugTextField extends TextField{
+
+   public function new() {   
+      super();
+      width = 600;
+      height = 600;
+      alpha = 0.5;
+      background = true;
+      backgroundColor = 0xFFFFFF;
+      visible = false;
+   }
+
+   public function trace(s : String) {
+     appendText(s);
+   }
+
+}
